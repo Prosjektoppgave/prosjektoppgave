5365151a2a5569cc9a3fdbcda5a84d03b157f19f
Kristine Hagen <hagenkristine@gmail.com>
2017-10-27 09:51:09 +0200

    Legger til xpress init

---
+++ functions/Xpress.java
@@ -0,0 +1,38 @@
+package functions;
+
+import com.dashoptimization.*;
+import java.lang.*;
+import java.util.ArrayList;
+import java.io.*;
+
+public class Xpress {
+
+    public static void runXpress() throws XPRMCompileException, IOException {
+        XPRM mosel;
+        XPRMModel mod;
+        Double lastSimulationTime = 0.0;
+        Double nextSimulationTime;
+        String currentTime;
+        ArrayList<String> moselOutput;
+
+        //Initialize model
+        mosel = new XPRM();
+
+        //Compile model
+        System.out.println("Compiling 'Subproblem.mos'");
+        mosel.compile("subproblem.mos");
+
+        //Load bim file
+        System.out.println("Loading 'Subproblem.bim'");
+        mod = mosel.loadModel("subproblem.bim");
+
+        //Execute model
+        System.out.println("Executing 'Subproblem.mos'");
+        mod.run();
+
+        //Stop if no solution is found
+        if(mod.getProblemStatus()!=mod.PB_OPTIMAL)
+            System.exit(1);
+    }
+
+}
--- mains/RunSimulation.java
+++ mains/RunSimulation.java
@@ -1,31 +1,32 @@
 package mains;
 
 import classes.Vehicle;
+import com.dashoptimization.XPRMCompileException;
 import functions.*;
 import org.json.JSONException;
 import classes.Station;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-public class Xpress {
+public class RunSimulation {
 
     private ArrayList<Integer> stationIdList;
     private ArrayList<Station> stations;
     private ArrayList<Vehicle> vehicles;
 
-    public static void main(String[] args) throws IOException, JSONException {
-        double currentTime = 8*60*60;                   //Current time in seconds
-        double stopTime = 20*60*60;                     //Stop time in seconds
-        Xpress xpress = new Xpress();                   //Read initial data
-        xpress.findCoordinationsAndDrivingTime();       //Find coordinations and driving times
-        xpress.writeTimeDependentInput(currentTime);    //Write time dependent input
+    public static void main(String[] args) throws IOException, JSONException, XPRMCompileException {
+        double currentTime = 8*60*60;                           //Current time in seconds
+        double stopTime = 20*60*60;                             //Stop time in seconds
+        RunSimulation runSimulation = new RunSimulation();      //Read initial data
+        //runSimulation.findCoordinationsAndDrivingTime();      //Find coordinations and driving times
+        //runSimulation.writeTimeDependentInput(currentTime);   //Write time dependent input
+        runSimulation.runXpress();                              //Run RunSimulation
     }
 
-    private Xpress() throws IOException, JSONException {
+    private RunSimulation() throws IOException, JSONException {
         this.stationIdList = ReadStationList.readStationIdList();
         this.stations = ReadDemandAndNumberOfBikes.simulatedDemand(stationIdList);
         this.vehicles = ReadVehicleInput.readVehicleInput();
@@ -39,6 +40,10 @@ public class Xpress {
     private void writeTimeDependentInput(double currentTime) throws FileNotFoundException, UnsupportedEncodingException {
         TimeDependentInput.printTimeDependentInput(stations, vehicles, currentTime);
     }
-    
+
+    private void runXpress() throws IOException, XPRMCompileException {
+        Xpress.runXpress();
+    }
+
 }
 
